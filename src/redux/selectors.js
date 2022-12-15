80daca34c3d0f8ef3caaae4652d50732c6eeb7a8
Надія Ткач <nadiya77723@gmail.com>
2022-12-15 12:33:07 +0200

    додає ще два селектора

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,6 +2,8 @@ import { createSelector } from '@reduxjs/toolkit';
 
 export const selectFilter = state => state.filter.value;
 export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
 
 export const selectFilterContacts = createSelector(
   [selectContacts, selectFilter],
